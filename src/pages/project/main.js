2217cb4fa49f58d226b03ae5c91eace1f95e66d9
大姐姐 <wzxielulu@163.com>
2021-09-07 15:42:35 +0800

    项目查询搜索页

--- src/pages/project/main.js
+++ src/pages/project/main.js
@@ -1,12 +1,43 @@
 import React from 'react'
 
+import { List, Picker, Button } from 'antd-mobile'
+import { createForm } from 'rc-form'
 
-export default class Project extends React.Component {
+import Frame from '../../frame'
+
+class Project extends React.Component {
 
   render() {
     return (
-      <div>Project</div>
+      <Frame title="项目查询">
+        <div className="page-customer mt52 pl24 pr24">
+          <List>
+            <Picker 
+              data={[
+                {
+                  label: '2013',
+                  value: '2013',
+                },
+                {
+                  label: '2014',
+                  value: '2014',
+                },
+              ]} 
+              cols={1} 
+              extra="请选择所属公司"
+            >
+              <List.Item arrow="horizontal">所属公司</List.Item>
+            </Picker>
+          </List>
+          <Button type="primary" className="mt52">
+            查询
+          </Button>
+        </div>
+      </Frame>
     )
   }
 
 }
+
+const SearchForm = createForm()(Project)
+export default SearchForm
