a569451035241bb89896b5ae2fc25f8b6fd68ded
大姐姐 <wzxielulu@163.com>
2021-09-24 14:59:20 +0800

    空页面

--- src/component/empty/main.js
+++ src/component/empty/main.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import IMG from './empty.png'
 export default class Empty extends React.Component {
 
   render() {
     const {text} = this.props
-    return (<div>
-      {text || '查询结果为空'}
+    return (<div className="text-center">
+      <img style={{width: '200px', height: '200px'}} src={IMG} alt="空"/>
+      <div className="mt12 gray">
+        {text || '查询结果为空'}
+      </div>
     </div>)
   }
 }
